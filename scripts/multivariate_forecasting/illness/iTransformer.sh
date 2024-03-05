aee3de0cbf06525d882ca34e259ea39d6fd3091d
Anggito02 <anggito_anju@yahoo.com>
2024-03-05 15:48:48 +0700

    add illness dataset runner

---
+++ scripts/multivariate_forecasting/illness/iTransformer.sh
@@ -0,0 +1,24 @@
+export CUDA_VISIBLE_DEVICES=0
+
+model_name=iTransformer
+
+python -u run.py \
+  --is_training 1 \
+  --root_path ./dataset/illness/ \
+  --data_path national_illness.csv \
+  --model_id ILI_36_24 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 36 \
+  --pred_len 24 \
+  --e_layers 3 \
+  --enc_in 321 \
+  --dec_in 321 \
+  --c_out 321 \
+  --des 'Exp' \
+  --d_model 512 \
+  --d_ff 512 \
+  --batch_size 32 \
+  --learning_rate 0.001 \
+  --itr 1
